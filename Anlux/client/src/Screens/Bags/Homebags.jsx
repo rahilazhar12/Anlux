cba7ce639cbdef3913a21fd811815fa80134022a
rahilazhar12 <158289635+rahilazhar12@users.noreply.github.com>
2024-06-16 17:14:37 +0500

    Update Homebags.jsx

--- Anlux/client/src/Screens/Bags/Homebags.jsx
+++ Anlux/client/src/Screens/Bags/Homebags.jsx
@@ -42,7 +42,7 @@ const Homebags = () => {
   }, []);
   useEffect(() => {
     // Fetch all products
-    fetch('http://localhost:8000/api/glasses-get')
+    fetch('http://api.anluxuries.com/api/glasses-get')
       .then(response => response.json())
       .then(data => {
         setGlasses(data);
