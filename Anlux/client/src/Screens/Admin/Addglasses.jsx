3288f8ce8135a79585e4fb242444b8f5d2eb2c52
rahilazhar12 <158289635+rahilazhar12@users.noreply.github.com>
2024-06-16 16:47:55 +0500

    Add files via upload

--- Anlux/client/src/Screens/Admin/Addglasses.jsx
+++ Anlux/client/src/Screens/Admin/Addglasses.jsx
@@ -31,7 +31,7 @@ const AddGlasses = () => {
         form.append('newPrice', formData.newPrice);
 
         try {
-            const response = await fetch('http://localhost:8000/api/glasses-post', {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/glasses-post`, {
                 method: 'POST',
                 body: form
             });
