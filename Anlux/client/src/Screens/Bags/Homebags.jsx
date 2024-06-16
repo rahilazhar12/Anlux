055add320e784549b7e1d1dd9d003e9f9bebd44e
rahilazhar12 <158289635+rahilazhar12@users.noreply.github.com>
2024-06-16 17:18:04 +0500

    Add files via upload

--- Anlux/client/src/Screens/Bags/Homebags.jsx
+++ Anlux/client/src/Screens/Bags/Homebags.jsx
@@ -42,7 +42,7 @@ const Homebags = () => {
   }, []);
   useEffect(() => {
     // Fetch all products
-    fetch('http://api.anluxuries.com/api/glasses-get')
+    fetch('http://localhost:8000/api/glasses-get')
       .then(response => response.json())
       .then(data => {
         setGlasses(data);
